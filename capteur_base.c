c928a213e336e48e607d31165d91dfb5782675df
Marie-Claire56 <marieclairezaina2@gmail.com>
2024-11-12 00:59:57 +0200

    Update capteur_base.CO2

--- capteur_base.c
+++ capteur_base.c
@@ -4,6 +4,10 @@
 int main() {
     int msgid;
     struct msg_buffer message;
+    //déclaration  des variables capteur
+    float CO2_level = 400.5;// niveau de CO2 en PPM (exemple)
+    float temperature = 22.3;// temperature en degrés Celsius (exemple)
+    float humidity = 45.2; // humidity (exemple)
 
     // Create or access the message queue
     msgid = msgget(CAPTEUR_CO2, 0666 | IPC_CREAT);
@@ -12,9 +16,11 @@ int main() {
         exit(1);
     }
 
-    // Prepare the message
-    message.msg_type = 1; // Set message type
-    strcpy(message.msg_text, "Hello from the sender!");
+    // Préparation du message
+    message.msg_type = 1; // type de message, nécessaire pour l'envoi 
+
+    //formatage des données capteur dans msg_text
+    snprintf(message.msg_text, sizeof(message.msg_text),"CO2: %.1f ppm,temperature:%.1f Celsius, humidity: %.1f%% ", CO2_level, temperature, humidity);
 
     // Send the message
     if (msgsnd(msgid, &message, sizeof(message.msg_text), 0) == -1) {
